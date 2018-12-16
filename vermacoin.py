e75bbea374068f31339124d5a470ae16afbf89bc
Ankit Verma <all.hail.ankit@gmail.com>
2018-12-16 15:34:41 -0800

    update local email and commit with correct email

--- vermacoin.py
+++ vermacoin.py
@@ -42,6 +42,6 @@ for i in range(0, num_of_blocks_to_add):
   block_to_add = next_block(previous_block)
   blockchain.append(block_to_add)
   previous_block = block_to_add
-  # Tell everyone about it!
+  # Tell everyone about it!!
   print "Block #{} has been added to the blockchain!".format(block_to_add.index)
   print "Hash: {}\n".format(block_to_add.hash) 
